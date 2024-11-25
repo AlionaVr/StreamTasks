63d797931698efdd7804ded026da0cceba5d165c
AlionaVr <alonavershilo@yandex.by>
2024-11-25 13:14:32 +0300

    Another way to avoid making a mistake when dividing by zero

--- src/t1_Calculator/Calculator.java
+++ src/t1_Calculator/Calculator.java
@@ -7,11 +7,17 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> x / y;
+    BinaryOperator<Integer> divide = (x, y) -> {
+        if (y == 0) {
+            System.out.println("Error: Division by zero");
+            return 0;
+        }
+        return x / y;
+    };
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
     Predicate<Integer> isZero = x -> x == 0;
-    Consumer<Object> println = System.out::println;
+    Consumer<Integer> println = System.out::println;
 }
 
--- src/t1_Calculator/Main.java
+++ src/t1_Calculator/Main.java
@@ -6,12 +6,9 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        try {
-            int c = calc.divide.apply(a, b);
-            calc.println.accept(c);
-        } catch (ArithmeticException e) {
-            calc.println.accept(e.getMessage());
-        }
+        int c = calc.divide.apply(a, b);
+        calc.println.accept(c);
+
 
         /*Ошибка возникала из-за деления на ноль. Чтобы избежать ошибки, я добавила выбрасывание исключения.
          Также можно было сделать это через условный оператор:
@@ -24,5 +21,11 @@ public class Main {
         Кроме того, я поменяла integer на object в строчке  Consumer<Object> println = System.out::println,
         чтобы была возможность выводить строчки */
 
+ /*try {
+            int c = calc.divide.apply(a, b);
+           calc.println.accept(c);
+        } catch (ArithmeticException e) {
+            calc.println.accept(e.getMessage());
+        }*/
     }
 }
