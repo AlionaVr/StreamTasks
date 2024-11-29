7f9ada70b2be36fdf5838d5d9d2dc176c4fb64c3
AlionaVr <alonavershilo@yandex.by>
2024-11-29 23:54:21 +0300

    creation of files and directories, FileWriter write the text in file

---
+++ serialization/games/CreationFiles.java
@@ -0,0 +1,65 @@
+package serialization.games;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CreationFiles {
+    static StringBuilder textDate = new StringBuilder();
+
+    public static void main(String[] args) {
+        CreationFiles creator = new CreationFiles();
+        String basePath = "F:\\Projects\\JavaCoreTasks\\serialization\\games";
+        creator.makeDir(basePath, "src");
+        creator.makeDir(basePath, "res");
+        creator.makeDir(basePath, "savegames");
+        creator.makeDir(basePath, "temp");
+
+        String srcPath = basePath + "\\src";
+        creator.makeDir(srcPath, "main");
+        creator.makeDir(srcPath, "test");
+
+        String mainPath = srcPath + "\\main";
+        creator.createFile(mainPath, "Main.java");
+        creator.createFile(mainPath, "Utils.java");
+
+        String resPath = basePath + "\\res";
+        creator.makeDir(resPath, "drawables");
+        creator.makeDir(resPath, "vectors");
+        creator.makeDir(resPath, "icons");
+
+        String tempPath = basePath + "\\temp";
+        creator.createFile(tempPath, "temp.txt");
+
+        String filePath = tempPath + "\\temp.txt";
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write(textDate.toString());
+            writer.flush();
+            System.out.println("Лог записан в файл temp.txt");
+        } catch (IOException e) {
+            System.out.println("Ошибка записи в файл: " + e.getMessage());
+        }
+
+    }
+
+    protected void makeDir(String path, String name) {
+        File dir = new File(path, name);
+        if (dir.mkdir()) {
+            textDate.append("Создан катaлог ").append(name).append("\n");
+        }
+    }
+
+    protected void createFile(String path, String name) {
+        File newFile = new File(path, name);
+        try {
+            if (newFile.createNewFile())
+                textDate.append("создан файл ").append(name).append("\n");
+        } catch (IOException e) {
+            textDate.append(e.getMessage());
+        }
+
+    }
+
+}
